fb520f4d19c245cd1f4ba3cfe5dd75c4d928a84c
Naveen <naveen.balamuri@gmail.com>
2018-11-23 09:41:34 +0800

    formatting response as json

--- src/io/nuls/review/contract/ReviewContract.java
+++ src/io/nuls/review/contract/ReviewContract.java
@@ -1,66 +1,64 @@
-package io.nuls.review.contract;
+package io.nuls.contract.review;
 
 import io.nuls.contract.sdk.*;
 import io.nuls.contract.sdk.annotation.Payable;
 import io.nuls.contract.sdk.annotation.Required;
 import io.nuls.contract.sdk.annotation.View;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class ReviewContract implements Contract {
 
-    Map<String, List<Review>> reviews = new HashMap<>();
-
-    @View
-    public List<Review> getProductReviews(@Required String productId){
-        Utils.require(!productId.isEmpty(),"Product ID is required");
-        if(!reviews.containsKey(productId)){
-            Utils.revert("No Reviews found for the product!!");
-        }
-        return reviews.get(productId);
-    }
+    Map<String,List<Review>> reviews = new HashMap<String, List<Review>>();
 
     @Payable
     public Review writeReview(@Required String productId,@Required String reviewComments){
-        Utils.require(!productId.isEmpty(),"Product ID is required for this operation");
-        Utils.require(!reviewComments.isEmpty(),"Review comments are required");
+        Utils.require(!productId.isEmpty(),"Product ID is required");
+        Utils.require(!reviewComments.isEmpty(),"Review Comments are required");
         Address address = Msg.sender();
-        ReviewContract.Review review = new Review(reviewComments,productId,address.toString());
+        Review review = new Review(productId,reviewComments,address.toString());
+
         List<Review> reviewList = reviews.get(productId);
-        if( null == reviewList){
-            reviewList = new ArrayList<>();
+        if(null == reviewList){
+            reviewList = new ArrayList<Review>();
         }
         reviewList.add(review);
         reviews.put(productId,reviewList);
-        Utils.emit(new WriteReviewEvent(productId,reviewComments));
+        Utils.emit(new WriteReviewEvent(productId,reviewComments,address.toString()));
         return review;
     }
 
-    private static class Review{
+    @View
+    public List<Review> getReviews(@Required String productId){
+        Utils.require(!productId.isEmpty(),"Product is is required");
+        return reviews.get(productId);
+    }
 
-        private String comments;
+    @View
+    public String getAllProductIds(){
+        Set<String> productIds = reviews.keySet();
+        List<String> ids = new ArrayList<String>();
+
+        String res = "[";
+        for(String id : productIds){
+            ids.add(id);
+            res = res + "\""+id+"\"" +",";
+        }
+        res = res.substring(0,res.length()-1);
+        res = res + "]";
+        return res;
+    }
+
+    private class Review{
+
         private String productId;
+        private String comments;
         private String writer;
 
-        public Review(String comments, String productId,String writter) {
-            this.comments = comments;
+        public Review(String productId, String comments, String writer) {
             this.productId = productId;
-            this.writer = writter;
-        }
-
-        public Review(String comments, String productId) {
-            this(comments,productId,null);
-        }
-
-        public String getComments() {
-            return comments;
-        }
-
-        public void setComments(String comments) {
             this.comments = comments;
+            this.writer = writer;
         }
 
         public String getProductId() {
@@ -71,32 +69,60 @@ public class ReviewContract implements Contract {
             this.productId = productId;
         }
 
-        public String getWritter() {
+        public String getComments() {
+            return comments;
+        }
+
+        public void setComments(String comments) {
+            this.comments = comments;
+        }
+
+        public String getWriter() {
             return writer;
         }
 
-        public void setWritter(String writter) {
-            this.writer = writter;
+        public void setWriter(String writer) {
+            this.writer = writer;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            Review review = (Review) o;
+
+            if (!productId.equals(review.productId)) return false;
+            if (!comments.equals(review.comments)) return false;
+            return writer.equals(review.writer);
+        }
+
+        @Override
+        public int hashCode() {
+            int result = productId.hashCode();
+            result = 31 * result + comments.hashCode();
+            result = 31 * result + writer.hashCode();
+            return result;
         }
 
         @Override
         public String toString() {
-            return "Review{" +
-                    " comments='" + comments + '\'' +
-                    ", productId='" + productId + '\'' +
-                    ", writer='" + writer + '\'' +
-                    '}';
+            return
+                    "{\"productId\""+":"+ "\""+productId +"\""+
+                            ","+"\"comments\""+":" + "\""+comments+ "\""+
+                            "," +"\"writer\"" +":"+ "\""+writer+"\""+"}";
         }
     }
 
-    private static class WriteReviewEvent implements Event {
-
+    private class WriteReviewEvent implements Event{
         private String productId;
         private String reviewComments;
+        private String writer;
 
-        public WriteReviewEvent(String productId, String reviewComments) {
+        public WriteReviewEvent(String productId, String reviewComments,String writer) {
             this.productId = productId;
             this.reviewComments = reviewComments;
+            this.writer = writer;
         }
 
         public String getProductId() {
@@ -115,12 +141,41 @@ public class ReviewContract implements Contract {
             this.reviewComments = reviewComments;
         }
 
+        public String getWriter() {
+            return writer;
+        }
+
+        public void setWriter(String writer) {
+            this.writer = writer;
+        }
+
         @Override
         public String toString() {
-            return "WriteReviewEvent{" +
-                    "productId='" + productId + '\'' +
+            return "productId='" + productId + '\'' +
                     ", reviewComments='" + reviewComments + '\'' +
+                    ", writer='" + writer + '\'' +
                     '}';
         }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            WriteReviewEvent that = (WriteReviewEvent) o;
+
+            if (!productId.equals(that.productId)) return false;
+            if (!reviewComments.equals(that.reviewComments)) return false;
+            return writer.equals(that.writer);
+        }
+
+        @Override
+        public int hashCode() {
+            int result = productId.hashCode();
+            result = 31 * result + reviewComments.hashCode();
+            result = 31 * result + writer.hashCode();
+            return result;
+        }
     }
+
 }
